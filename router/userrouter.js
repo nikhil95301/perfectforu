07487d094acc0a99b58bbb61315b07bf8ebc9798
nikhil95301 <nikhil9530175113@gmail.com>
2020-09-04 18:55:40 +0530

    try catch

--- router/userrouter.js
+++ router/userrouter.js
@@ -3,7 +3,11 @@ const express = require('express')
 const router = express.Router()
 router.post('/signup',async(req,res) =>{
     const user = await User(req.body)
-    await user.save()
-    res.status(201).send(user)
+ try {  await user.save()
+    res.status(201).send(user)}
+    catch(e)
+    {
+        console.log(e)
+    }
 })
 module.exports = router
