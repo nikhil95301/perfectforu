9b83800dcad6b24f3d0c1174eb8b43941cba4b01
nikhil95301 <nikhil9530175113@gmail.com>
2020-09-09 22:26:42 +0530

    some changes

--- router/userrouter.js
+++ router/userrouter.js
@@ -1,9 +1,13 @@
 const User = require('../model/usermodel')
 const express = require('express')
 const router = express.Router()
+
 router.post('/signup',async(req,res) =>{
     const user = await User(req.body)
- try {  await user.save()
+ try {  
+    console.log(user)
+    await user.save()
+    
     res.status(201).send(user)}
     catch(e)
     {
